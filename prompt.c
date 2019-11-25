33b6d985781bf5b6942ea4462dfc6bc950fc88ec
danny099 <dalmariv01@gmail.com>
2019-11-25 15:11:42 +0000

    realice 0.1 and 0.2

--- prompt.c
+++ prompt.c
@@ -4,21 +4,20 @@
  *
  * Return: Always 0.
  */
-int main()
+int main(void)
 {
+	pid_t son, status;
 	size_t size;
 	int read, i;
 	char *string, *token, *toc[16];
-	
-	while(1)
+
+	while (read != EOF)
 	{
 		write(1, "$ ", 2);
-		string = (char *) malloc (size);
-		read = getline (&string, &size, stdin);
+		string = (char *) malloc(size);
+		read = getline(&string, &size, stdin);
 		if (read == -1)
-		{
-			printf("error");
-		}
+			read = EOF;
 		token = strtok(string, " \n");
 		for (i = 0; token != NULL; i++)
 		{
@@ -26,7 +25,11 @@ int main()
 			token = strtok(NULL, " \n");
 		}
 		toc[i] = NULL;
-		execve(toc[0], toc, NULL);
+		son = fork();
+		if (son == 0)
+			execve(toc[0], toc, NULL);
+		else
+			wait(&status);
 	}
 	return (0);
 }
