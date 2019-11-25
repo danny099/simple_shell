6b5589a5aff63da0c224ddf3761b4c167bf0092a
danny099 <dalmariv01@gmail.com>
2019-11-25 23:27:18 +0000

    prompt

--- prompt.c
+++ prompt.c
@@ -11,25 +11,28 @@ int main(void)
 	int read, i;
 	char *string, *token, *toc[16];
 
-	while (read != EOF)
+	while (1)
 	{
 		write(1, "$ ", 2);
 		string = (char *) malloc(size);
 		read = getline(&string, &size, stdin);
 		if (read == -1)
-			read = EOF;
-		token = strtok(string, " \n");
-		for (i = 0; token != NULL; i++)
-		{
+		  {
+		    printf("\n");
+		    exit(0);
+		  }
+		if (read > 1)
+		  {
+		    token = strtok(string, " \n");
+		    for (i = 0; token != NULL; i++)
+		      {
 			toc[i] = token;
 			token = strtok(NULL, " \n");
-		}
-		toc[i] = NULL;
-		son = fork();
-		if (son == 0)
-			execve(toc[0], toc, NULL);
-		else
-			wait(&status);
+		      }
+		    toc[i] = NULL;
+		    son = fork();
+		    son == 0 ? execve(toc[0], toc, NULL) : wait(&status);
+		  }
 	}
 	return (0);
 }
